e42fbf31de3238a4fff42ff9f913c24c724a05d7
Mathieu Tarral <mathieu.tarral@gmail.com>
2018-11-14 21:33:40 +0100

    remove useless cb_data arg

--- vmidbg/debugcontext.py
+++ vmidbg/debugcontext.py
@@ -67,7 +67,7 @@ class DebugContext:
                 self.target_pid = vmi.dtb_to_pid(self.target_dtb)
                 cb_data['interrupted'] = True
 
-        reg_event = RegEvent(X86Reg.CR3, RegAccess.W, cb_on_cr3_load, data=cb_data)
+        reg_event = RegEvent(X86Reg.CR3, RegAccess.W, cb_on_cr3_load)
         self.vmi.register_event(reg_event)
         self.vmi.resume_vm()
 
