bbc824d05a800e12b77685f8214f4279f55fc421
Mathieu Tarral <mathieu.tarral@gmail.com>
2019-02-16 17:38:53 +0100

    attach Linux task by CR3 load event

--- vmidbg/linuxdebugcontext.py
+++ vmidbg/linuxdebugcontext.py
@@ -1,7 +1,8 @@
 import logging
 import re
 
-from libvmi import AccessContext, TranslateMechanism
+from libvmi import AccessContext, TranslateMechanism, X86Reg
+from libvmi.event import RegEvent, RegAccess
 
 
 class LinuxThread:
@@ -18,10 +19,16 @@ class LinuxTaskDescriptor:
     def __init__(self, desc_addr, vmi):
         self.vmi = vmi
         self.addr = desc_addr
-        self.dtb = self.vmi.read_32_va(self.addr + self.vmi.get_offset('linux_pgd'), 0)
+        self.mm = self.vmi.read_addr_va(self.addr + self.vmi.get_offset('linux_mm'), 0)
         self.name = self.vmi.read_str_va(self.addr + self.vmi.get_offset('linux_name'), 0)
         self.pid = self.vmi.read_32_va(self.addr + self.vmi.get_offset('linux_pid'), 0)
-        self.mm = self.vmi.read_addr_va(self.addr + self.vmi.get_offset('linux_mm'), 0)
+        # task_struct->mm->pgd
+        if self.mm:
+            dtb_addr = self.vmi.read_addr_va(self.mm + self.vmi.get_offset('linux_pgd'), 0)
+            self.dtb = self.vmi.read_32_va(dtb_addr, 0)
+        else:
+            # kernel thread
+            self.dtb = 0
         task_addr = self.vmi.read_addr_va(self.addr + self.vmi.get_offset('linux_tasks'), 0)
         self.next_desc = task_addr - self.vmi.get_offset('linux_tasks')
 
@@ -32,12 +39,13 @@ class LinuxTaskDescriptor:
 class LinuxDebugContext:
 
     def __init__(self, vmi, process):
+        self.log = logging.getLogger(__class__.__name__)
         self.vmi = vmi
         self.target_name = process
         self.target_desc = None
         self.threads = [LinuxThread(1)]
         # misc: print kernel base address
-        logging.info('kernel base: @%s', hex(self.vmi.translate_ksym2v('start_kernel')))
+        self.log.info('kernel base: @%s', hex(self.vmi.translate_ksym2v('start_kernel')))
 
     def attach(self):
         # 1 - pause to get a consistent memory access
@@ -52,13 +60,48 @@ class LinuxDebugContext:
                 logging.debug(desc)
             raise RuntimeError('Could not find process')
         if len(found) > 1:
-            logging.warning('Found multiple processes matching %s, picking the first match', self.target_name)
+            logging.warning('Found %s processes matching "%s", picking the first match ([%s])',
+                            len(found), self.target_name, found[0].pid)
         self.target_desc = found[0]
         # 3 - check if kernel thread (not supported)
         if self.target_desc.mm == 0:
             raise RuntimeError('intercepting kernel threads is not supported')
-        # 4 - wait for our process to be scheduled
-        # TODO
+        # 4 - wait for our process to be scheduled (CR3 load)
+        cb_data = {
+            'interrupted': False
+        }
+
+        def cb_on_cr3_load(vmi, event):
+            # TODO find process by matching CR3 directly
+            # it doesn't work for now
+            # desc = self.dtb_to_desc(event.cffi_event.reg_event.value)
+            # self.log.info('intercepted %s', desc.name)
+            # if desc.dtb == self.target_desc.dtb:
+            #     vmi.pause_vm()
+            #     cb_data['interrupted'] = True
+
+            # find pid
+            pid = self.vmi.dtb_to_pid(event.cffi_event.reg_event.value)
+            # find desc
+            found = [desc for desc in self.list_processes() if desc.pid == pid]
+            if len(found) > 1:
+                raise RuntimeError('Multiple tasks have same PID')
+            desc = found[0]
+            self.log.info('intercepted [%s]: %s', desc.pid, desc.name)
+            if desc.pid == self.target_desc.pid:
+                self.vmi.pause_vm()
+                cb_data['interrupted'] = True
+
+        reg_event = RegEvent(X86Reg.CR3, RegAccess.W, cb_on_cr3_load)
+        self.vmi.register_event(reg_event)
+        self.vmi.resume_vm()
+
+        while not cb_data['interrupted']:
+            self.vmi.listen(1000)
+        # clear queue
+        self.vmi.listen(0)
+        # clear event
+        self.vmi.clear_event(reg_event)
 
     def detach(self):
         self.vmi.resume_vm()
@@ -86,3 +129,10 @@ class LinuxDebugContext:
 
     def get_current_thread(self):
         return self.threads[0]
+
+    def dtb_to_desc(self, dtb):
+        for desc in self.list_processes():
+            self.log.debug('dtb_to_desc: %s, %s <=> %s', desc.name, hex(desc.dtb), hex(dtb))
+            if desc.dtb == dtb:
+                return desc
+        raise RuntimeError('Could not find task descriptor for DTB {}'.format(hex(dtb)))
