3151aed5314f0db0c39b382e99c1ce6ee0d9b840
Mathieu Tarral <mathieu.tarral@gmail.com>
2019-02-24 23:04:20 +0100

    list threads

--- vmidbg/windowsdebugcontext.py
+++ vmidbg/windowsdebugcontext.py
@@ -8,23 +8,48 @@ from libvmi.event import RegEvent, RegAccess
 
 class WindowsThread:
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, thread_list_entry, vmi, rekall):
+        self.vmi = vmi
+        self.rekall = rekall
+        self.rekall_thread = self.rekall['$STRUCTS']['_ETHREAD'][1]
+        unique_thread_off = self.rekall['$STRUCTS']['_CLIENT_ID'][1]['UniqueThread'][0]
+        self.addr = thread_list_entry - self.rekall_thread['ThreadListEntry'][0]
+        self.id = self.vmi.read_addr_va(self.addr + self.rekall_thread['Cid'][0] + unique_thread_off, 0)
+        self.next_entry = self.vmi.read_addr_va(self.addr + self.rekall_thread['ThreadListEntry'][0], 0)
+        self.start_addr = self.vmi.read_addr_va(self.addr + self.rekall_thread['StartAddress'][0], 0)
+        self.win32_start_addr = self.vmi.read_addr_va(self.addr + self.rekall_thread['Win32StartAddress'][0], 0)
 
     def is_alive(self):
         return True
 
+    def __str__(self):
+        return "[{}] - addr: {}, start_address: {}, win32_start_address: {}".format(self.id, hex(self.addr), hex(self.start_addr), hex(self.win32_start_addr))
+
 
 class WindowsTaskDescriptor:
 
-    def __init__(self, task_addr, vmi):
+    def __init__(self, task_addr, vmi, rekall):
         self.vmi = vmi
+        self.rekall = rekall
+        self.rekall_task = self.rekall['$STRUCTS']['_EPROCESS'][1]
+        thread_list_off = self.rekall_task['ThreadListHead'][0]
         self.addr = task_addr - self.vmi.get_offset('win_tasks')
         self.dtb = self.vmi.read_32_va(self.addr + self.vmi.get_offset('win_pdbase'), 0)
         self.pid = self.vmi.read_32_va(self.addr + self.vmi.get_offset('win_pid'), 0)
         self.name = self.vmi.read_str_va(self.addr + self.vmi.get_offset('win_pname'), 0)
+        self.thread_head = self.vmi.read_addr_va(self.addr + thread_list_off, 0)
         self.next_task = self.vmi.read_addr_va(self.addr + self.vmi.get_offset('win_tasks'), 0)
         self.next_desc = self.next_task - self.vmi.get_offset('win_tasks')
+
+    def list_threads(self):
+        thread_list_entry = self.thread_head
+        while True:
+            desc = WindowsThread(thread_list_entry, self.vmi, self.rekall)
+            yield desc
+            # read next thread
+            thread_list_entry = desc.next_entry
+            if thread_list_entry == self.thread_head:
+                break
 
     def __str__(self):
         return "[{}] {} @{}".format(self.pid, self.name, hex(self.addr))
@@ -35,10 +60,12 @@ class WindowsDebugContext:
     def __init__(self, vmi, process):
         self.log = logging.getLogger(__class__.__name__)
         self.vmi = vmi
+        self.rekall = None
+        with open(self.vmi.get_rekall_path()) as f:
+            self.rekall = json.load(f)
         self.process = process
         self.target_name = process
         self.target_desc = None
-        self.threads = [WindowsThread(1)]
         # misc: print kernel base address
         # small hack with rekall JSON profile to get the kernel base address
         # LibVMI should provide an API to query it
@@ -52,7 +79,6 @@ class WindowsDebugContext:
             self.log.info('kernel base: @%s', hex(ps_head_va - ps_head_rva))
 
     def attach(self):
-
         # 1 - pause to get a consistent memory access
         self.vmi.pause_vm()
         # 2 - find our target name in process list
@@ -60,47 +86,23 @@ class WindowsDebugContext:
         pattern = re.escape(self.target_name)
         found = [desc for desc in self.list_processes() if re.match(pattern, desc.name)]
         if not found:
-            logging.debug('%s not found in process list:', self.target_name)
+            self.log.debug('%s not found in process list:', self.target_name)
             for desc in self.list_processes():
-                logging.debug(desc)
+                self.log.debug(desc)
             raise RuntimeError('Could not find process')
         if len(found) > 1:
-            logging.warning('Found %s processes matching "%s", picking the first match ([%s])',
+            self.log.warning('Found %s processes matching "%s", picking the first match ([%s])',
                             len(found), self.target_name, found[0].pid)
         self.target_desc = found[0]
-        # 4 - wait for our process to be scheduled (CR3 load)
-        cb_data = {
-            'interrupted': False
-        }
-
-        def cb_on_cr3_load(vmi, event):
-            found = [desc for desc in self.list_processes() if desc.dtb == event.cffi_event.reg_event.value]
-            if not found:
-                raise RuntimeError('Cannot find currently scheduled process')
-            if len(found) > 2:
-                raise RuntimeError('Found multiple tasks matching same DTB')
-            desc = found[0]
-            self.log.info('intercepted %s', desc.name)
-            if desc.dtb == self.target_desc.dtb:
-                vmi.pause_vm()
-                cb_data['interrupted'] = True
-
-        reg_event = RegEvent(X86Reg.CR3, RegAccess.W, cb_on_cr3_load)
-        self.vmi.register_event(reg_event)
-        self.vmi.resume_vm()
-
-        while not cb_data['interrupted']:
-            self.vmi.listen(1000)
-        # clear queue
-        self.vmi.listen(0)
-        # clear event
-        self.vmi.clear_event(reg_event)
+        # 4 - enumerate threads
+        for thread in self.list_threads():
+            self.log.info(thread)
 
     def list_processes(self):
         head_task = self.vmi.translate_ksym2v('PsActiveProcessHead')
         task_addr = self.vmi.read_addr_va(head_task, 0)
         while True:
-            desc = WindowsTaskDescriptor(task_addr, self.vmi)
+            desc = WindowsTaskDescriptor(task_addr, self.vmi, self.rekall)
             yield desc
             # read next task
             task_addr = desc.next_task
@@ -109,14 +111,15 @@ class WindowsDebugContext:
         # Idle process ? (Window XP)
         if self.vmi.get_winver() == VMIWinVer.OS_WINDOWS_XP:
             idle_desc_addr = self.vmi.read_addr_ksym('PsIdleProcess')
-            desc = WindowsTaskDescriptor(idle_desc_addr + self.vmi.get_offset('win_tasks'), self.vmi)
+            desc = WindowsTaskDescriptor(idle_desc_addr + self.vmi.get_offset('win_tasks'), self.vmi,
+                                         self.rekall)
             yield desc
 
     def list_threads(self):
-        return self.threads
+        return self.target_desc.list_threads()
 
     def get_current_thread(self):
-        return self.threads[0]
+        return next(self.target_desc.list_threads())
 
     def get_access_context(self, address):
         return AccessContext(TranslateMechanism.PROCESS_PID,
