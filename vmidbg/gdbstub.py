257be701f6eeed1f1b6c1947b5a38309b92b3f26
Mathieu Tarral <mathieu.tarral@gmail.com>
2019-02-24 23:16:42 +0100

    fix logging in GDBStub

--- vmidbg/gdbstub.py
+++ vmidbg/gdbstub.py
@@ -80,7 +80,7 @@ class GDBStub():
 
     def __init__(self, conn, addr):
         _, client_port = addr
-        self.log = logging.getLogger('client-{}'.format(client_port))
+        self.log = logging.getLogger(__class__.__name__)
         self.sock = conn
         self.addr = addr
         self.sock.setblocking(True)
@@ -177,7 +177,7 @@ class GDBStub():
         cmd, cmd_data = chr(packet_data[0]), packet_data[1:]
         try:
             gdb_cmd = GDBCmd(cmd)
-            logging.info('command %s: %s', gdb_cmd.value, gdb_cmd.name)
+            self.log.info('command %s: %s', gdb_cmd.value, gdb_cmd.name)
             handler = self.cmd_to_handler[gdb_cmd]
         except (ValueError, KeyError):
             self.log.info('unknown command {}'.format(cmd))
